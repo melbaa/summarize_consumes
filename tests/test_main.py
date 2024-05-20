b9a3ce63ab1548ad48feec002a763a47300c3445
Creohex <paradise.source@gmail.com>
2024-05-20 17:12:01 +0300

    add tests for new models

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 import pytest
 
+from melbalabs.summarize_consumes.main import ConsumablesAccumulator
+from melbalabs.summarize_consumes.main import ConsumablesEntry
+from melbalabs.summarize_consumes.main import ConsumableStore
 from melbalabs.summarize_consumes.main import Currency
+from melbalabs.summarize_consumes.main import NAME2ITEMID
 
 
 def test_currency():
@@ -54,3 +58,58 @@ def test_currency_init(source, expected):
 def test_currency_init_negative(invalid_value):
     with pytest.raises(ValueError):
         Currency(invalid_value)
+
+
+@pytest.mark.parametrize(
+    ('amount', 'price', 'expected_total'),
+    [
+        (0, 0, 0),
+        (5, 7, 35),
+    ],
+)
+def test_consumable_store(amount, price, expected_total):
+    name = 'bla'
+    obj = ConsumableStore(name, amount, Currency(price))
+    assert obj.total_price == expected_total
+
+
+def test_consumable_entry():
+    entry = ConsumablesEntry('name123', deaths=5)
+
+    assert entry.name == 'name123'
+    assert entry.deaths == 5
+    assert not entry.consumables
+    assert entry.total_spent == 0
+
+    entry.add_consumable(ConsumableStore('item1', amount=1, price=Currency(5)))
+    assert len(entry.consumables) == 1
+    assert entry.total_spent == 5
+
+    entry.add_consumable(ConsumableStore('item2',
+                                         amount=3,
+                                         price=Currency.from_string('15g')))
+    assert len(entry.consumables) == 2
+    assert entry.total_spent == Currency.from_string('45g5c')
+
+
+def test_consumable_accumulator(app):
+    app.pricedb.data[NAME2ITEMID['Elixir of the Mongoose']] = 30_000
+    app.pricedb.data[NAME2ITEMID['Flask of the Titans']] = 1_000_000
+    app.player['Psykhe']['Elixir of the Mongoose'] = 3
+    app.player['Psykhe']['Flask of the Titans'] = 1
+    app.player['Random']['Wizard Oil'] = 5
+    app.death_count['Psykhe'] = 1
+    app.death_count['Random'] = 42
+
+    accumulator = ConsumablesAccumulator(app.player, app.pricedb, app.death_count)
+    assert accumulator.death_count['Psykhe'] == 1
+    assert not accumulator.data
+
+    accumulator.calculate()
+    assert len(accumulator.data) == 2
+
+    player = next(e for e in accumulator.data if e.name == 'Psykhe')
+    assert player.name == 'Psykhe'
+    assert player.deaths == 1
+    assert len(player.consumables) == 2
+    assert player.total_spent == 1_090_000
