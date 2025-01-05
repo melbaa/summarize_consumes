738e0f09b7dd3ea7c83ad5ce17cd42699fbbf321
melbaa <none@no-reply>
2025-01-06 00:36:33 +0200

    more deps

--- src/melbalabs/summarize_consumes/package.py
+++ src/melbalabs/summarize_consumes/package.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
-VERSION = "2024.1082"
+VERSION = "2025.1083"
 PROJECT_NAME = __package__
 PROJECT_METADATA = importlib.metadata.metadata(PROJECT_NAME)
 PROJECT_URL = PROJECT_METADATA['Project-URL']
